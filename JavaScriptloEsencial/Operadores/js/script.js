d4270288d5afddf5645272a479cd5c63aabf76d2
Gerardo Hernandez <ghd.gmail.com>
2017-05-23 01:01:59 -0500

    Operadores JS

---
+++ JavaScriptloEsencial/Operadores/js/script.js
@@ -0,0 +1,14 @@
+var numero1 = 4;
+var numero2 = 3;
+
+var resultado1 = (numero1 + numero2) * 5;
+
+var numero3 = 10;
+var numero4 = 7;
+
+var resultado2 = (numero3 - numero4) * 9;
+
+//Imprimiendo variables resultados
+
+document.getElementById('resultado1').innerHTML = resultado1;
+document.getElementById('resultado2').innerHTML = resultado2;
