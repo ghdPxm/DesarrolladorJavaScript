77085b5cd55648993a5ffebc4853b0efd2888b32
Gerardo Hernandez <ghd.gmail.com>
2017-05-23 00:54:37 -0500

    Variables JS

---
+++ JavaScriptloEsencial/Variables/js/script.js
@@ -0,0 +1,37 @@
+//Variables indicadores
+
+var m;
+
+m = 20;
+
+var precio_1;
+
+precio_1 = 35000;
+
+var nombre = "Jaun";
+var nomBre = "Pedro";
+
+color = "Rojo"; // Definicion inplicita
+
+var total; // Valor undefined
+
+//Tipos de Variables principales
+
+var animal = "Oso";
+var estudiante = 'Miguel';
+
+var numero1 = 434.5;
+var numero2 = 3246;
+
+//Imprimiendo Variables
+
+document.getElementById('var1').innerHTML = m;
+document.getElementById('var2').innerHTML = precio_1;
+document.getElementById('var3').innerHTML = nombre;
+document.getElementById('var4').innerHTML = nomBre;
+document.getElementById('var5').innerHTML = color;
+document.getElementById('var6').innerHTML = total;
+document.getElementById('var7').innerHTML = animal;
+document.getElementById('var8').innerHTML = estudiante;
+document.getElementById('var9').innerHTML = numero1;
+document.getElementById('var10').innerHTML = numero2;
