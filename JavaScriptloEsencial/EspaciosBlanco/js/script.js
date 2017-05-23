06b767140651ea4c2ce0529c21a733ada0cf3c7f
Gerardo Hernandez <ghd.gmail.com>
2017-05-23 01:53:18 -0500

    Espacios en blanco

---
+++ JavaScriptloEsencial/EspaciosBlanco/js/script.js
@@ -0,0 +1,11 @@
+//Espacios en blanco
+
+var color1 = "rojo";
+var color2 = "azul";
+
+document.getElementById('color1').innerHTML = color1;
+document.getElementById('color2').innerHTML = color2;
+
+//Saltos de linea
+
+document.getElementById('miDiv').innerHTML = "Hola soy un DIV" + " con contenido dinamico" ;
