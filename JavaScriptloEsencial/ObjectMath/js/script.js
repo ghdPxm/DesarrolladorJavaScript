cbd47e113e4d3dc509b06b157af11c1c345b3a3a
Gerardo Hernandez <ghd.gmail.com>
2017-05-27 03:28:34 -0500

    ObjectMath

---
+++ JavaScriptloEsencial/ObjectMath/js/script.js
@@ -0,0 +1,18 @@
+//Objeto Math
+
+var a = Math.PI; //Constante PI
+var b = Math.cos(90); //Metodo cos() permite calcular el coseno de un numero
+var c = Math.sin(180); //Metodo sin() permite calcular el seno de un numero
+var d = Math.max(1, 20); //Metodo max() permite calcular el numero maximo entre varios numeros
+var e = Math.min(13, 3); //Metodo min() permite calcular el numero minimo entre varios numeros
+var f = Math.pow(2, 4); //Metodo pow() permite calcular la potencia de un numero
+var g = Math.sqrt(144); //Metodo sqrt() permite calcular la raiz cuadrada de un numero
+
+
+document.getElementById('m1').innerHTML = a;
+document.getElementById('m2').innerHTML = b;
+document.getElementById('m3').innerHTML = c;
+document.getElementById('m4').innerHTML = d;
+document.getElementById('m5').innerHTML = e;
+document.getElementById('m6').innerHTML = f;
+document.getElementById('m7').innerHTML = g;
