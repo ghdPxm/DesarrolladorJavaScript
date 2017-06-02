481337942a51e5e9c2b79950f94c70edf2a029fe
Gerardo Hernandez <ghd.gmail.com>
2017-06-02 08:00:41 -0500

    Uso de Console Log

---
+++ JavaScriptloEsencial/ConsoleLog/js/script.js
@@ -0,0 +1,12 @@
+var numero1 = 45;
+var numero2 = 97;
+var numero3 = 63;
+var animales = ["Zorro", "Oso", "Gorila"];
+
+var resultado = (numero1 * numero2) / numero3;
+
+console.log("Arreglo de animales: " + animales );
+console.log("Numero1: " + numero1);
+console.log("Numero2: " + numero2);
+console.log("Numero3: " + numero3);
+console.log("Resultado: " + resultado);
