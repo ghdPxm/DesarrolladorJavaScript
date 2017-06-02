e47c7a7b7cf60bb8f7d2603dac5899e8074be8e5
Gerardo Hernandez <ghd.gmail.com>
2017-06-02 08:08:53 -0500

    Uso de Cosole Table

---
+++ JavaScriptloEsencial/ConsoleTable/js/script.js
@@ -0,0 +1,28 @@
+var notas = [
+  [4.5, 3.5, 2.1, 4.6, 3.6],
+  [3.2, 4.3, 5.0, 2.5, 2.9],
+  [4.4, 1.5, 2.7, 3.5, 3.9]
+];
+
+var productos = [
+{
+  codigo: "001",
+  nombre: "Camisa",
+  precio: 50000
+}
+,
+{
+  codigo: "002",
+  nombre: "Gorra",
+  precio: 40000
+}
+,
+{
+  codigo: "003",
+  nombre: "Zapatos",
+  precio: 60000
+}
+];
+
+console.table(productos);
+console.table(notas);
